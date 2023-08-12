83abbea928a37b3450ff4a20833b68e34f8e8365
Gesem Martinez <ghmartinezmontes@gmail.com>
2023-08-12 01:42:32 -0600

    Add playRound function

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 function getComputerChoice (){
-    let options = ["Rock", "Paper", "Scissors"];
+    let options = ["rock", "paper", "scissors"];
     let randomIndex; 
     let computerChoice;
 
     randomIndex = Math.floor(Math.random() * 3);
     computerChoice = options[randomIndex];
 
-    console.log(computerChoice);
+    return computerChoice;
+}
+
+function playRound(computerChoice, playerChoice){
+    let result;
+    if(computerChoice === playerChoice){
+        return result = "Draw";
+    }
+
+    switch(playerChoice){
+        case 'rock':
+            if(computerChoice === 'paper'){
+                result = 'You Lose! Paper beats Rock';
+            } else if(computerChoice === 'scissors'){
+                result = 'You Win! Rock beats Scissors';
+            }
+            break;
+        case 'paper':
+            if(computerChoice === 'rock'){
+                result = 'You Win! Paper beats Rock';
+            } else if(computerChoice === 'scissors'){
+                result = 'You Lose! Scissors beat Paper';
+            }
+            break;
+        case 'scissors':
+            if(computerChoice === 'rock'){
+                result = 'You Lose! Rock beats Scissors';
+            } else if(computerChoice === 'paper'){
+                result = 'You Win! Scissors beat Paper';
+            }
+            break;
+    }
+
+    return result;
 }
