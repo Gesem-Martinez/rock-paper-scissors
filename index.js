f144edde4aafbc37b7a59fb6e56af1519e4735b0
Gesem Martinez <ghmartinezmontes@gmail.com>
2023-09-02 10:29:22 -0600

    Add scores and resetGame function

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const computerButtons = document.querySelector(".comp-buttons").querySelectorAll("button");
+const playerButtons = document.querySelector(".player-buttons").querySelectorAll("button");
+
+playerButtons.forEach((button) => {
+    let buttonId = button.id;
+
+    button.addEventListener("click", () => {
+        computerButtons.forEach((button) => {
+            button.classList.remove('active');
+        });
+
+        playerButtons.forEach((button) => {
+            button.classList.remove('active');
+        })
+
+        playRound(buttonId);
+
+        if(playerScore == 5 || computerScore == 5){
+            resetGame();
+        }
+    });
+});
+
+
+
+function resetGame(){
+    if(playerScore > computerScore){
+        document.getElementById('game-result').textContent = "Player Wins!";
+    } else {
+        document.getElementById('game-result').textContent = "Computer Wins!";
+    }
+
+    playerScore = 0;
+    computerScore = 0;
+
+    computerButtons.forEach((button) => {
+        button.classList.remove('active');
+    });
+
+    playerButtons.forEach((button) => {
+        button.classList.remove('active');
+    });
+    document.getElementById("player-score").textContent = playerScore;
+    document.getElementById("computer-score").textContent = computerScore;
+}
+
+
 function getComputerChoice (){
     let options = ["rock", "paper", "scissors"];
     let randomIndex; 
@@ -10,70 +60,45 @@ function getComputerChoice (){
 }
 
 function playRound(buttonId){
-    let result;
-    let playerChoice = buttonId;
+    let result = "It's a Draw!";
+    let playerChoice = buttonId.split('-')[1];
     let computerChoice = getComputerChoice();
 
-    if(computerChoice === playerChoice){
-        result = "It's a Draw!";
-    }
+    document.getElementById(buttonId).classList.add('active')
+    document.getElementById(("comp" + "-" + computerChoice)).classList.add('active');
 
     switch(playerChoice){
         case 'rock':
             if(computerChoice === 'paper'){
                 result = 'You Lose! Paper beats Rock';
+                computerScore++;
             } else if(computerChoice === 'scissors'){
                 result = 'You Win! Rock beats Scissors';
+                playerScore++;
             }
             break;
         case 'paper':
             if(computerChoice === 'rock'){
                 result = 'You Win! Paper beats Rock';
+                playerScore++;
             } else if(computerChoice === 'scissors'){
                 result = 'You Lose! Scissors beat Paper';
+                computerScore++;
             }
             break;
         case 'scissors':
             if(computerChoice === 'rock'){
                 result = 'You Lose! Rock beats Scissors';
+                computerScore++;
             } else if(computerChoice === 'paper'){
                 result = 'You Win! Scissors beat Paper';
+                playerScore++;
             }
             break;
     }
 
-    console.log(result);
-    return result;
+    document.getElementById("player-score").textContent = playerScore;
+    document.getElementById("computer-score").textContent = computerScore;
 }
 
-const playerButtons = document.querySelector(".player-buttons").querySelectorAll("button");
-playerButtons.forEach((button) => {
-    let buttonId = button.id.split('-')[1];
-    button.addEventListener("click", () => playRound(buttonId));
-});
-
-/*
-function game(){
-    let winCounter = 0;
-    let loseCounter = 0;
-
-    for(let i = 0; i < 5; i++){
-        let roundResult = playRound();
-        if(roundResult[4] === 'W'){
-            winCounter++;
-        } else if (roundResult[4] === 'L'){
-            loseCounter++;
-        }
-    }
-
-    if(winCounter > loseCounter){
-        console.log("You Win! Congrats!");
-    } else if(winCounter === loseCounter){
-        console.log("It's a Draw!");
-    } else {
-        console.log("You Lose! Good luck next time!");
-    }
-}
-
-game();*/
 //playRound();
